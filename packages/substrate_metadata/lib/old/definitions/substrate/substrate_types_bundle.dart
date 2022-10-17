14b6f58c5223a880801fce7fbcc78d373c6d9714
Kawaljeet Singh <kawal741536@gmail.com>
2022-10-17 12:49:25 +0530

    formatting

--- packages/substrate_metadata/lib/old/definitions/substrate/substrate_types_bundle.dart
+++ packages/substrate_metadata/lib/old/definitions/substrate/substrate_types_bundle.dart
@@ -2,5 +2,5 @@ import '../../legacy_types.dart' as legacy;
 import 'substrate_types.dart';
 import 'substrate_types_alias.dart';
 
-final substrateTypesBundle =
-    legacy.LegacyTypesBundle(types: substrateTypes, typesAlias: substrateTypesAlias);
+final substrateTypesBundle = legacy.LegacyTypesBundle(
+    types: substrateTypes, typesAlias: substrateTypesAlias);
